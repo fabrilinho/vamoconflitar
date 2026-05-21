c61da2799e9362a5380c981a83334c4fa034ad30
Your Name <you@example.com>
2026-05-21 20:03:02 -0300

    Inserindo bug

--- ehprimo.py
+++ ehprimo.py
@@ -4,8 +4,8 @@ import sys
 def eh_primo(num):
     for i in range(2,num//2):
         if num % i ==0:
-            return False
-    return True
+            return True
+    return False
 
 if __name__ == "__main__":
     num1 = int(sys.argv[1])
